7dfe4e014c8094be1160459cad26e3d8fd89aa20
KarthikJVN <jvnkarthik94@gmail.com>
2019-02-24 22:56:45 -0600

    added basic run scripts

---
+++ insight_testsuite/run_tests.sh
@@ -0,0 +1,3 @@
+python3 src/pharmacy-counting.py input/itcont.txt output/top_cost_drug.txt
+python3 src/pharmacy-counting.py insight_testsuite/tests/test_medium/input/dec_cc_data_100k.txt insight_testsuite/tests/test_medium/output/top_cost_drug_100k.txt
+python3 src/pharmacy-counting.py insight_testsuite/tests/test_2/input/dec_cc_data_10k.txt insight_testsuite/tests/test_2/output/top_cost_drug_10k.txt
